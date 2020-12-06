50ed51ca4c819306a90e896750c1a817e4e84e84
Alessandro1979-itac <sandromuniz27@hotmail.com>
2020-12-06 18:31:27 -0400

    chore: create project setup

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  lintOnSave: false,
+	lintOnSave: false,
+	css: {
+		loaderOptions: {
+			sass: {
+				additionalData: `
+					@import "@/styles/functions";
+					@import "@/styles/colors";
+				`,
+			},
+		},
+	},
 };
