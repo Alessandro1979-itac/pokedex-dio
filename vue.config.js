9983e46569c1146659c895dad3a8633b5e89d071
Alessandro1979-itac <sandromuniz27@hotmail.com>
2020-12-06 18:37:22 -0400

    chore: create pokedex panels

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 module.exports = {
 	lintOnSave: false,
+
 	css: {
 		loaderOptions: {
 			sass: {
 				additionalData: `
 					@import "@/styles/functions";
-					@import "@/styles/colors";
+          @import "@/styles/colors";
+          @import "@/styles/variables";
 				`,
 			},
 		},
